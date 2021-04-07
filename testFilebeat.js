ecab290c15d0e3fdb8070042f43a0ac92e89aaf6
justindo <namdn@mirabo.com.vn>
2021-04-07 11:46:00 +0700

    111111

--- testFilebeat.js
+++ testFilebeat.js
@@ -1,43 +1,31 @@
 const fs = require('fs');
 
-let stringTest = `2021-04-05T11:56:21.207 INFO JanusGatewaySfu - create wss://webrtc-sfu.mirabo.tech:8989
-2021-04-05T11:56:21.224 INFO SocketClusterServer - start starting...
-2021-04-05T11:56:21.225 INFO TypeOrmDbContext - connectMysql
-2021-04-05T11:56:21.228 INFO SocketClusterServer - listening on port: 3000
-2021-04-05T11:56:21.724 INFO TypeOrmDbContext - connectMysql connected
-2021-04-05T11:56:21.972 INFO MotBackEnd - onServerUp
-2021-04-05T11:56:21.972 INFO MotBackEnd - registerRpcHandlers
-2021-04-05T11:56:21.977 INFO InterServiceTransceiver - initInterServiceComm init sockets
-2021-04-05T11:56:21.979 INFO TranscribeManager - subscribeAppEvent  'group_call_events' 
-2021-04-05T11:56:21.979 INFO InterServiceTransceiver - subscribeTopic group_call_events
-2021-04-05T11:56:21.980 INFO InterServiceTransceiver - subscribeTopic zmqSubSock subscribe to topic group_call_events
-2021-04-05T11:56:21.980 INFO InterServiceTransceiver - saveTopicConsFunc group_call_events
-2021-04-05T11:56:21.980 INFO GroupCallHandler - subscribeAppEvent  'user_manage_events' 
-2021-04-05T11:56:21.980 INFO InterServiceTransceiver - subscribeTopic user_manage_events
-2021-04-05T11:56:21.980 INFO InterServiceTransceiver - subscribeTopic zmqSubSock subscribe to topic user_manage_events
-2021-04-05T11:56:21.980 INFO InterServiceTransceiver - saveTopicConsFunc user_manage_events
-2021-04-05T11:56:21.981 INFO ShareScreenHandler - subscribeAppEvent  'user_manage_events' 
-2021-04-05T11:56:21.981 INFO InterServiceTransceiver - subscribeTopic user_manage_events
-2021-04-05T11:56:21.981 INFO InterServiceTransceiver - saveTopicConsFunc user_manage_events
-2021-04-05T11:56:21.981 INFO InterServiceTransceiver - startConsumeMessage
-2021-04-05T11:56:30.285 INFO SocketClusterServer - onAgSocketConnection 4ZSeYokIT7eKgFmNAAAA
-2021-04-05T11:56:30.286 INFO SocketClusterServer - registerRPCHandlers 4ZSeYokIT7eKgFmNAAAA
-2021-04-05T11:56:30.286 INFO SocketClusterServer - registerRPCHandler 4ZSeYokIT7eKgFmNAAAA layout_manage_rpc
-2021-04-05T11:56:30.286 INFO SocketClusterServer - registerRPCHandler 4ZSeYokIT7eKgFmNAAAA user_manage_rpc
-2021-04-05T11:56:30.286 INFO SocketClusterServer - registerRPCHandler 4ZSeYokIT7eKgFmNAAAA transcriptRpc
-2021-04-05T11:56:30.286 INFO SocketClusterServer - registerRPCHandler 4ZSeYokIT7eKgFmNAAAA group_call_rpc
-2021-04-05T11:56:30.286 INFO SocketClusterServer - registerRPCHandler 4ZSeYokIT7eKgFmNAAAA share_screen_rpc
-2021-04-05T11:56:30.287 INFO SocketClusterServer - registerRPCHandler 4ZSeYokIT7eKgFmNAAAA webrtc_rpc
-2021-04-05T11:56:33.485 INFO Authentication - checkAuthenticaion eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjE5Iiwicm9sZXMiOltdLCJ0eXBlIjoiaG9sb2xlbiIsInVzZXJuYW1lIjoianVzdGluZG8iLCJpYXQiOjE2MTc1OTc4NjYsImV4cCI6MTYxNzU5ODc2Nn0.JJGUgso8761gT0WMBTcH7mbI38h-5jv3JyBpcmAJSU8
-2021-04-05T11:56:33.486 WARN Authentication - checkAuthenticaion HOLOLEN NOT CHECK AUTHENTICAION
-2021-04-05T11:56:33.486 DEBUG UserManageApp - handleRpc { requestId: 1, method: 'deleteGroup', params: { groupId: 3 } }
-2021-04-05T11:56:33.486 INFO GroupManager - deleteGroups 3
-2021-04-05T11:56:33.492 ERROR GroupManager - deleteGroups not found
-2021-04-05T11:56:48.834 INFO SocketClusterServer - onAgSocketDisconnect 4ZSeYokIT7eKgFmNAAAA
-2021-04-05T11:56:48.834 INFO MotBackEnd - onUserDisconnected 19
-2021-04-05T11:56:48.834 DEBUG UserManageApp - handleRpc { method: 'disconnect', params: {} }
-2021-04-05T11:56:48.835 INFO InterServiceTransceiver - initInterServiceComm user_manage_events {"jsonrpc":"2.0","method":"user_leave","params":{"userId":"19"}}
-2021-04-05T11:56:48.838 DEBUG InterServiceTransceiver - startConsumeMessage interServiceMess user_manage_events {"jsonrpc":"2.0","method":"user_leave","params":{"userId":"19"}}`
+let stringTest = `
+2021-04-07T04:35:57.245 INFO SocketClusterServer registerRPCHandler 8sL1Dr7eH5c7P7nkAAFN share_screen_rpc
+2021-04-07T04:35:57.245 INFO SocketClusterServer registerRPCHandler 8sL1Dr7eH5c7P7nkAAFN webrtc_rpc
+2021-04-07T04:36:58.223 INFO SocketClusterServer onAgSocketDisconnect null
+2021-04-07T04:36:58.223 INFO SocketClusterServer onAgSocketDisconnect 8sL1Dr7eH5c7P7nkAAFN
+2021-04-07T04:38:23.668 INFO Speech2Text none Restarting
+2021-04-07T04:38:25.424 ERROR Speech2Text API error:  8 RESOURCE_EXHAUSTED: Too many pending audio packets. Audio should be streamed at a rate that approximates real time, and performance is best with larger, less-frequent audio packets. For example, 32 KB packets at a rate of 2 packets per-second.
+2021-04-07T04:38:25.424 INFO Transcriber Stop 27 61 ja-JP
+2021-04-07T04:38:25.424 INFO JanusAudioListener none stop
+2021-04-07T04:38:25.424 INFO WebRTC none Stop webrtc listener
+2021-04-07T04:38:25.426 INFO Speech2Text none Stop
+2021-04-07T04:38:25.428 INFO WebRTC Change connection state closed
+2021-04-07T04:39:19.074 INFO SocketClusterServer onAgSocketConnection tCJi9VgalMqTIfJoAAFO
+2021-04-07T04:39:19.074 INFO SocketClusterServer registerRPCHandlers tCJi9VgalMqTIfJoAAFO
+2021-04-07T04:39:19.074 INFO SocketClusterServer registerRPCHandler tCJi9VgalMqTIfJoAAFO layout_manage_rpc
+2021-04-07T04:39:19.074 INFO SocketClusterServer registerRPCHandler tCJi9VgalMqTIfJoAAFO user_manage_rpc
+2021-04-07T04:39:19.074 INFO SocketClusterServer registerRPCHandler tCJi9VgalMqTIfJoAAFO transcriptRpc
+2021-04-07T04:39:19.074 INFO SocketClusterServer registerRPCHandler tCJi9VgalMqTIfJoAAFO group_call_rpc
+2021-04-07T04:39:19.075 INFO SocketClusterServer registerRPCHandler tCJi9VgalMqTIfJoAAFO share_screen_rpc
+2021-04-07T04:39:19.075 INFO SocketClusterServer registerRPCHandler tCJi9VgalMqTIfJoAAFO webrtc_rpc
+2021-04-07T04:39:31.485 INFO Authentication none checkAuthenticaion eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjQ1Iiwicm9sZXMiOltdLCJ0eXBlIjoiaG9sb2xlbiIsInVzZXJuYW1lIjoiVGFuaSIsImlhdCI6MTYxNzc2NzkwMSwiZXhwIjoxNjE3NzY4ODAxfQ.aOOoa9_K7y90OmAayhbqRP8oZEu90d21CSHzKuo1WQ0
+2021-04-07T04:39:31.485 WARN Authentication 45 checkAuthenticaion HOLOLEN NOT CHECK AUTHENTICAION
+2021-04-07T04:39:31.485 INFO FolderManager 45 getFoldersTree
+2021-04-07T04:39:50.179 INFO SocketClusterServer onAgSocketDisconnect null
+2021-04-07T04:39:50.179 INFO SocketClusterServer onAgSocketDisconnect tCJi9VgalMqTIfJoAAFO
+`
 let stringFormat = stringTest.split('\n')
 
 stringFormat.forEach(e => {
